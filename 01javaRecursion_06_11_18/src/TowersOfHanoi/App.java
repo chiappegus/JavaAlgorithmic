204e3af7c03caf0bc75f517cd26a330e3e4b3642
gchiappe <gchiappe@magyp.gob.ar>
2018-11-07 12:07:57 -0300

    se usa imagen de towers

---
+++ 01javaRecursion_06_11_18/src/TowersOfHanoi/App.java
@@ -0,0 +1,11 @@
+package TowersOfHanoi;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		Algorithm algorithm = new Algorithm();
+		algorithm.solveHanoiProblem(4, 'A', 'B', 'C');
+		
+	}
+}
