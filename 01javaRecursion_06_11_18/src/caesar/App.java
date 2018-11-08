64a12d3679d4aaaa35edbc4db8de482d60f0b223
Gustavo <chiappegus@gmail.com>
2018-11-07 23:05:09 -0300

    busqueda binaria y busqueda lineal Array.sort()

---
+++ 01javaRecursion_06_11_18/src/caesar/App.java
@@ -0,0 +1,48 @@
+package caesar;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		CaesarCipher caesarCipher = new CaesarCipher();
+		
+		String plainText = "If he had anything confidential to say he wrote it in cipher that is by so changing the order of the letters of the alphabet that not a word could be made out";
+		String cipherText = caesarCipher.encrypt(plainText);
+		System.out.println(cipherText);
+		
+		cipherText = "VKDQQRQGHILQHGWKHTXDQWLWBRILQIRUPDWLRQSURGXFHGEBDVRXUFHIRUHADPSOHWKHTXDQWLWBLQDPHVVDJHEBDIRUPXODVLPLODUWRWKHHTXDWLRQWKDWGHILQHVWKHUPRGBQDPLFHQWURSBLQSKBVLFVLQLWVPRVWEDVLFWHUPVVKDQQRQVLQIRUPDWLRQDOHQWURSBLVWKHQXPEHURIELQDUBGLJLWVUHTXLUHGWRHQFRGHDPHVVDJHWRGDBWKDWVRXQGVOLNHDVLPSOHHYHQREYLRXVZDBWRGHILQHKRZPXFKLQIRUPDWLRQLVLQDPHVVDJHLQDWWKHYHUBGDZQRIWKHLQIRUPDWLRQDJHWKLVGLJLWLCLQJRILQIRUPDWLRQRIDQBVRUWZDVDUHYROXWLRQDUBVWHSKLVSDSHUPDBKDYHEHHQWKHILUVWWRXVHWKHZRUGELWVKRUWIRUELQDUBGLJLWDVZHOODVGHILQLQJLQIRUPDWLRQVKDQQRQDQDOBCHGWKHDELOLWBWRVHQGLQIRUPDWLRQWKURXJKDFRPPXQLFDWLRQVFKDQQHOKHIRXQGWKDWDFKDQQHOKDGDFHUWDLQPDALPXPWUDQVPLVVLRQUDWHWKDWFRXOGQRWEHHAFHHGHGWRGDBZHFDOOWKDWWKHEDQGZLGWKRIWKHFKDQQHOVKDQQRQGHPRQVWUDWHGPDWKHPDWLFDOOBWKDWHYHQLQDQRLVBFKDQQHOZLWKDORZEDQGZLGWKHVVHQWLDOOBSHUIHFWHUURUIUHHFRPPXQLFDWLRQFRXOGEHDFKLHYHGEBNHHSLQJWKHWUDQVPLVVLRQUDWHZLWKLQWKHFKDQQHOVEDQGZLGWKDQGEBXVLQJHUURUFRUUHFWLQJVFKHPHVWKHWUDQVPLVVLRQRIDGGLWLRQDOELWVWKDWZRXOGHQDEOHWKHGDWDWREHHAWUDFWHGIURPWKHQRLVHULGGHQVLJQDOWRGDBHYHUBWKLQJIURPPRGHPVWRPXVLFFGVUHOBRQHUURUFRUUHFWLRQWRIXQFWLRQDPDMRUDFFRPSOLVKPHQWRITXDQWXPLQIRUPDWLRQVFLHQWLVWVKDVEHHQWKHGHYHORSPHQWRIWHFKQLTXHVWRFRUUHFWHUURUVLQWURGXFHGLQTXDQWXPLQIRUPDWLRQDQGWRGHWHUPLQHMXVWKRZPXFKFDQEHGRQHZLWKDQRLVBTXDQWXPFRPPXQLFDWLRQVFKDQQHORUZLWKHQWDQJOHGTXDQWXPELWVTXELWVZKRVHHQWDQJOHPHQWKDVEHHQSDUWLDOOBGHJUDGHGEBQRLVH";
+		
+		plainText = caesarCipher.decrypt(cipherText);
+		System.out.println(plainText);
+                
+                
+                System.out.println(caesarCipher.gustavo("{}}"));
+                
+                //solo faltaria los primos :
+                
+                    //                def primo3(n):
+                    //	verificar= False
+                    //	contador=0
+                    //	for x in range(1,n+1):
+                    //		if(n%x==0):
+                    //				contador = contador +1 
+                    //				#return -1
+                    //		if (contador >= 3):
+                    //        		verificar=True
+                    //        		break #esto rompe el for!!		
+                    //	if(contador==2 or  verificar==False):
+                    //				
+                    //				return("es primo")
+                    //				#return -1
+                    //	else:
+                    //			return('NO es primo')	
+                    //
+                    //
+                    //		
+                    //
+                    //print(primo3(17))
+                    //                
+                
+		
+	}
+}
---
+++ 01javaRecursion_06_11_18/src/caesar/Constants.java
@@ -0,0 +1,12 @@
+package caesar;
+
+public class Constants {
+
+	private Constants() {
+		
+	}
+	
+	public static final String ALPHABET = " ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	public static final String CARACTERES = "{}[]";
+	public static final int KEY = 3;
+}
