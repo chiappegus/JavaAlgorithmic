dbc95ce9b56e85f6cbe6c61c3d8cc2e350232c8f
Gustavo <chiappegus@gmail.com>
2018-11-07 22:22:58 -0300

    solucion al merge :)

--- 01javaRecursion_06_11_18/src/TowersOfHanoi/Algorithm.java
+++ 01javaRecursion_06_11_18/src/TowersOfHanoi/Algorithm.java
@@ -5,11 +5,11 @@ public class Algorithm {
 	public void solveHanoiProblem(int n, char rodFrom, char middleRod, char rodTo){
 		
 		if( n == 1 ){
-<<<<<<< HEAD
+
 			System.out.println("Plate 1 from " + rodFrom + " to " + rodTo);
-=======
-			System.out.println("Plate 1 ok from " + rodFrom + " to " + rodTo);
->>>>>>> 204e3af7c03caf0bc75f517cd26a330e3e4b3642
+
+			
+
 			return;
 		}
 		
